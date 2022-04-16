73a05e26c0bcd2157d097ce3d94e960aedf5c285
cylim0987 <cylim.dev@gmail.com>
2022-04-16 19:40:59 +0900

    019

--- SQL200.sql
+++ SQL200.sql
@@ -106,3 +106,129 @@ SELECT ename, sal, NVL(comm,0), NVL(sal+comm,3939)
 FROM emp
 WHERE deptno = 10;
 
+--010
+SELECT ename, sal, job, deptno
+FROM emp
+WHERE sal <= 1200;
+
+--011
+SELECT ename, sal
+FROM emp
+WHERE sal BETWEEN 1000 AND 3000;
+
+--BETWEEN 하한값 AND 상한값. 반대로 하면 검색되지 않는다.
+SELECT ename, sal
+FROM emp
+WHERE (sal >= 1000 AND sal <= 3000);
+
+SELECT ename, sal
+FROM emp
+WHERE (sal <1000 OR sal >3000);
+
+SELECT ename, sal
+FROM emp
+WHERE sal NOT BETWEEN 1000 AND 3000;
+
+SELECT ename, hiredate
+FROM emp
+WHERE hiredate BETWEEN '1982/01/01' AND '1982/12/31';
+
+--012 --%
+SELECT ename, sal
+FROM emp
+WHERE ename LIKE 'S%';
+
+SELECT ename, sal
+FROM emp
+WHERE ename LIKE '_M%';
+
+-- % : 0개 이상의 임의 문자와 일치, _ : 하나의 문자와 일치
+SELECT ename
+FROM emp
+WHERE ename LIKE '%G';
+
+--A를 포함하는.
+SELECT ename
+FROM emp
+WHERE ename LIKE '%A%';
+
+--013 . !=NULL 불가.
+SELECT ename, NVL(comm,0)
+FROM emp
+WHERE comm is null;
+
+--014
+SELECT ename, sal, job
+FROM emp
+WHERE job in ('SALESMAN', 'ANALYST', 'MANAGER')
+ORDER BY 2 asc, 3 desc;
+
+SELECT ename, sal, job
+FROM emp
+WHERE (job ='SALESMAN' or job='ANALYST' or job='MANAGER');
+
+SELECT ename, sal, job
+FROm emp
+WHERE job NOT in ('salesman', 'analyst', 'manager');
+
+SELECT ename, sal, job
+FROM emp
+WHERE (job != 'SALESMAN' and job !='ANALYST' and job != 'MANAGER');
+
+--015 (AND OR NOT)
+SELECT ename, sal, job
+FROM emp
+WHERE sal >= 1200 AND job ='SALESMAN';
+
+SELECT ename, sal, job
+FROM emp
+WHERE sal >= 1200 OR job ='ABCD';
+
+--016 UPPER LOWER INITCAP
+--UPPER:모두대문자 ,LOWER:소문자, INITCAP:첫번째만 대문자로 출력
+SELECT UPPER(ename), LOWER(ename), INITCAP(ename)
+FROM emp;
+
+SELECT ename, sal
+FROM emp
+WHERE LOWER(ename)='scott';
+
+SELECT ename, sal
+FROM emp
+WHERE INITCAP(ename)='Scott';
+
+SELECT ename, sal
+FROM emp
+WHERE UPPER(ename)='SCOTT';
+
+SELECT ename, sal
+FROM emp
+WHERE LOWER(ename)='scott';
+
+--017 SUBSTR -- SMITH, 1, 2 : 1은 추출을 시작할 위치. 2는 몇 개,
+-- 1,2,3,4,5,6 = 1,2,3, -3, -2, -1
+SELECT SUBSTR('SMITH', -3, 3)
+FROM DUAL;
+
+--018 문자열의 길이 출력(LENGTH)
+SELECT ename, LENGTH(ename), job, LENGTH(job)
+FROM emp;
+
+SELECT LENGTH('가나다라마')
+FROM DUAL;
+
+--019 문자에서 특정 철자의 위치 출력(INSTR)
+SELECT INSTR('SMITH', 'M')
+FROM DUAL;
+
+SELECT INSTR('abcdefg@naver.com', '@')
+FROM DUAL;
+
+SELECT INSTR('cylim.dev@gmail.com', 'v')
+FROM DUAL;
+
+SELECT SUBSTR('abcdefgh@naver.com', INSTR('abcdefgh@naver.com','@')+1)
+FROM DUAL;
+
+SELECT RTRIM(SUBSTR('abcdefgh@naver.com', INSTR('abcdefgh@naver.com','@') + 1) , '.com') as rtrim
+FROM DUAL;
